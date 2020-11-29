e4ac679ac6ef21700d0193929d9711280a7c4532
T41-Ch1 <dekabitasp@gmail.com>
2020-11-29 13:38:24 +0900

    [mod]decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SA_PASSPHRASE" \
---output $HOME/secrets/gcp_sa_key.json my_secret.json.gpg
+--output $HOME/secrets/gcp_sa_key.json gcp_sa_key.json.gpg
