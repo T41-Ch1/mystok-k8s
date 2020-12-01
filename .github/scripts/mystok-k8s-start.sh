090de34493ec01d53d6c4814213fe9b29c587cb5
T41-Ch1 <dekabitasp@gmail.com>
2020-12-01 18:32:44 +0900

    [update]mystok-k8s-start.sh - to apply hpa.yaml

--- .github/scripts/mystok-k8s-start.sh
+++ .github/scripts/mystok-k8s-start.sh
@@ -3,4 +3,6 @@ kubectl apply -f mystok-app-deployment.yaml
 kubectl apply -f mystok-app-service.yaml
 kubectl apply -f mystok-proxy-deployment.yaml
 kubectl apply -f mystok-proxy-service.yaml
+kubectl apply -f mystok-app-hpa.yaml
+kubectl apply -f mystok-proxy-hpa.yaml
 
