63e3d907a6cf856304d914cd5f11f9e00c5a0ac1
T41-Ch1 <dekabitasp@gmail.com>
2020-12-04 08:34:17 +0900

    update: add ingress,remove proxy

--- .github/scripts/mystok-k8s-start.sh
+++ .github/scripts/mystok-k8s-start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 kubectl apply -f mystok-app-deployment.yaml
 kubectl apply -f mystok-app-service.yaml
-kubectl apply -f mystok-proxy-deployment.yaml
-kubectl apply -f mystok-proxy-service.yaml
 kubectl apply -f mystok-app-hpa.yaml
-kubectl apply -f mystok-proxy-hpa.yaml
+kubectl apply -f mystok-ingress.yaml
+
 
