c8c5a9506bd06db70250df07dbfae3ebc6701c95
T41-Ch1 <dekabitasp@gmail.com>
2020-12-04 21:01:12 +0900

    update: to apply ssl_cert

--- .github/scripts/mystok-k8s-start.sh
+++ .github/scripts/mystok-k8s-start.sh
@@ -3,5 +3,5 @@ kubectl apply -f mystok-app-deployment.yaml
 kubectl apply -f mystok-app-service.yaml
 kubectl apply -f mystok-app-hpa.yaml
 kubectl apply -f mystok-ingress.yaml
-
+kubectl apply -f mystok-ssl-cert.yaml
 
