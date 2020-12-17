697825e6386ea0f3a3af1b6381b163a9350d2976
T41-Ch1 <dekabitasp@gmail.com>
2020-12-17 11:02:20 +0900

    update: to use encrypted cloudstorage credentials

---
+++ .github/scripts/decrypt_secret-cloud-storage.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$MYSTOK-CLOUDSTORAGE-SA-PASSPHRASE" \
+--output $HOME/secrets/mystok-cloudstorage-sa-key.json.gpg
