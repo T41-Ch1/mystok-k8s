94dec150df3018d386b56ab0d6c93865a79e952f
T41-Ch1 <dekabitasp@gmail.com>
2020-12-18 11:54:13 +0900

    fix: wrong command syntax

--- .github/scripts/decrypt_secret-cloud-storage.sh
+++ .github/scripts/decrypt_secret-cloud-storage.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CLOUDSTORAGE_SA_PASSPHRASE" \
---output $HOME/secrets/mystok-cloudstorage-sa-key.json.gpg
+--output $HOME/secrets/mystok-cloudstorage-sa-key.json mystok-cloudstorage-sa-key.json.gpg
