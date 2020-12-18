88dda1566c01b6a1d5b60281561770cc5b28d0a0
T41-Ch1 <dekabitasp@gmail.com>
2020-12-18 12:02:21 +0900

    change: check gpg version

--- .github/scripts/decrypt_secret-cloud-storage.sh
+++ .github/scripts/decrypt_secret-cloud-storage.sh
@@ -3,5 +3,6 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+gpg --version
 gpg --quiet --batch --yes --decrypt --passphrase="$CLOUDSTORAGE_SA_PASSPHRASE" \
 --output $HOME/secrets/mystok-cloudstorage-sa-key.json mystok-cloudstorage-sa-key.json.gpg
