a79ecfb6534a1f054db9f8e981bd8a54c46843ec
T41-Ch1 <dekabitasp@gmail.com>
2020-12-18 11:52:34 +0900

    fix: wrong env name...

--- .github/scripts/decrypt_secret-cloud-storage.sh
+++ .github/scripts/decrypt_secret-cloud-storage.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$CLOUDSTORAGE-SA-PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$CLOUDSTORAGE_SA_PASSPHRASE" \
 --output $HOME/secrets/mystok-cloudstorage-sa-key.json.gpg
